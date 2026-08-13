6a99d9aed04dbcc899822b74bd8d89ef5c3f95d2
Eugen417 <eugen417@gmail.com>
2026-08-13 18:06:26 +0300

    Update button.py

--- custom_components/ippon_ups_snmp/button.py
+++ custom_components/ippon_ups_snmp/button.py
@@ -4,7 +4,7 @@ from homeassistant.const import CONF_HOST, CONF_PORT, CONF_USERNAME, CONF_PASSWO
 from homeassistant.helpers.entity import EntityCategory
 
 from .const import DOMAIN, OID_BATTERY_TEST_CMD
-from .snmp_helper import set_snmp_data
+from .snmp_helper import async_set_snmp_data
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -13,19 +13,18 @@ async def async_setup_entry(hass, entry, async_add_entities):
     port = entry.data.get(CONF_PORT, 161)
     user = entry.data[CONF_USERNAME]
     key = entry.data[CONF_PASSWORD]
-    engine = hass.data[DOMAIN]["engine"]
 
     async_add_entities([
-        IpponTestButton(engine, host, port, user, key, "test_10sec", 2, "mdi:battery-sync"),
-        IpponTestButton(engine, host, port, user, key, "test_until_low", 3, "mdi:battery-minus-outline"),
-        IpponTestButton(engine, host, port, user, key, "test_by_time", 4, "mdi:timer-play-outline"),
-        IpponTestButton(engine, host, port, user, key, "test_cancel", 5, "mdi:battery-off-outline"),
-        IpponTestButton(engine, host, port, user, key, "clear_info", 6, "mdi:delete-restore")
+        IpponTestButton(hass, host, port, user, key, "test_10sec", 2, "mdi:battery-sync"),
+        IpponTestButton(hass, host, port, user, key, "test_until_low", 3, "mdi:battery-minus-outline"),
+        IpponTestButton(hass, host, port, user, key, "test_by_time", 4, "mdi:timer-play-outline"),
+        IpponTestButton(hass, host, port, user, key, "test_cancel", 5, "mdi:battery-off-outline"),
+        IpponTestButton(hass, host, port, user, key, "clear_info", 6, "mdi:delete-restore")
     ])
 
 class IpponTestButton(ButtonEntity):
-    def __init__(self, engine, host, port, user, key, trans_key, cmd_value, icon):
-        self.engine = engine
+    def __init__(self, hass_obj, host, port, user, key, trans_key, cmd_value, icon):
+        self.hass_obj = hass_obj
         self.host = host
         self.port = port
         self.user = user
@@ -40,4 +39,4 @@ class IpponTestButton(ButtonEntity):
         self._attr_device_info = {"identifiers": {(DOMAIN, host)}}
 
     async def async_press(self) -> None:
-        await set_snmp_data(self.engine, self.host, self.port, self.user, self.key, OID_BATTERY_TEST_CMD, self.cmd_value)
+        await async_set_snmp_data(self.hass_obj, self.host, self.port, self.user, self.key, OID_BATTERY_TEST_CMD, self.cmd_value)
