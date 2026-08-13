8f0e53b9c772a3ad26bff1c614fd2cd3126d7bd7
Eugen417 <eugen417@gmail.com>
2026-08-13 18:07:34 +0300

    Update __init__.py

--- custom_components/ippon_ups_snmp/__init__.py
+++ custom_components/ippon_ups_snmp/__init__.py
@@ -1,14 +1,9 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from pysnmp.hlapi.asyncio import SnmpEngine
 from .const import DOMAIN, PLATFORMS
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
-    
-    if "engine" not in hass.data[DOMAIN]:
-        hass.data[DOMAIN]["engine"] = await hass.async_add_executor_job(SnmpEngine)
-        
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
